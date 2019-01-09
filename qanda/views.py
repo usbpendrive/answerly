7b03198736664beebfab72e8947ee22098f59770
usbpendrive <ferry.fernando@gmail.com>
2019-01-09 10:54:59 +0700

    Django crispy forms and markdownify

--- qanda/views.py
+++ qanda/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from django.http.response import HttpResponseBadRequest
 
-# Create your views here.
+from qanda.forms import QuestionForm
+from qanda.models import Question
+
+
+class AskQuestionView(LoginRequiredMixin, CreateView):
+    form_class = QuestionForm
+    template_name = 'qanda/ask.html'
+
+    def get_initial(self):
+        return {
+            'user': self.request.user.id
+        }
+
+    def form_valid(self, form):
+        action = self.request.POST.get('action')
+
+        if action == 'SAVE':
+            return super().form_valid(form)
+        elif action == 'PREVIEW':
+            preview = Question(
+                question=form.cleaned_data['question'],
+                title=form.cleaned_data['title'])
+            ctx = self.get_context_data(preview=preview)
+            return self.render_to_response(context=ctx)
+        return HttpResponseBadRequest()
